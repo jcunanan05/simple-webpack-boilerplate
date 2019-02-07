856e949197ae83c5a33c82ca84b32f1d2dbd85c2
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-06 23:31:28 -0500

    Add Html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = ({ mode } = { mode: 'production' }) => {
   return {
@@ -6,6 +7,7 @@ module.exports = ({ mode } = { mode: 'production' }) => {
     entry: path.join(__dirname, 'src/index.js'),
     output: {
       filename: 'bundle.js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin()]
   };
 };
